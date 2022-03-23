58029c777eda47765eb66c8bc97443a922cd64fe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-03-23 03:20:57 +0000

    Deploy dev.rektdao.finance to github.com/RektDAO/RektDAO-frontend-DEV.git:gh-pages

---
+++ static/js/ru-messages.0204c393.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@scaffold-eth/react-app"]=this["webpackJsonp@scaffold-eth/react-app"]||[]).push([[10],{1214:function(s,p){s.exports={messages:{}}}}]);
+//# sourceMappingURL=ru-messages.0204c393.chunk.js.map
